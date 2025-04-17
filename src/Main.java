8a0ab3b67ce4e3f52934c65f8e37761b975ab304
LeoCoud <leonardocrower@gmail.com>
2025-04-16 21:19:40 -0600

    Person.java y Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Person persona = new Person("Leo", 20);
+        persona.presentYourself();
+    }
 }
